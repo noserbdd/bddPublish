1a4c8fc5609325c1bf9af5e41496f254a0fa9fb8
sebastian groetzner <sebastian.groetzner@noser.com>
2020-11-26 10:55:01 +0100

    Not so useful Class_Hierarchy added; some "dead" references removed

--- AutoTests/ApplicationSettings.cs
+++ AutoTests/ApplicationSettings.cs
@@ -4,8 +4,6 @@ namespace Driver_AutoTests
 {
     public static class ApplicationSettings
     {
-        public static string TestsVideoDirectory => ConfigurationManager.AppSettings["TestsVideoDirectory"];
-        public static string FfmpegFilePath => ConfigurationManager.AppSettings["FfmpegFilePath"];
         public static string HomePath => ConfigurationManager.AppSettings["HomePath"];
     }
 }
